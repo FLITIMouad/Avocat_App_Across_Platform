3319da638048f0d0abda2dfc98a24f96d6789436
FLITIMouad <64863283+FLITIMouad@users.noreply.github.com>
2022-03-23 17:00:25 +0100

    delete folder backend

--- backend/src/routes/usersRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import async_handler from "express-async-handler";
-import { authUser } from "../controllers/userController.js";
-const Router=express.Router();
-
-Router.post("/login",authUser)
-
-export default Router
--- backend/src/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const generateToken = (infoUser) => {
-  return jwt.sign({ infoUser }, process.env.JWT_TOKEN, {
-    expiresIn: "30d",
-  });
-};
-
-export default generateToken;
